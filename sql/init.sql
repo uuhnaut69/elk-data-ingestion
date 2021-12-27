e734d462345fa9615cfe03b69ca8a1c55403cef8
Tuan Nguyen <uuhnaut96@gmail.com>
2021-12-27 08:39:29 +0700

    Update sql statements

--- sql/init.sql
+++ sql/init.sql
@@ -3,48 +3,42 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE category (
     category_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255) UNIQUE NOT null,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	deleted_at TIMESTAMP
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE author (
     author_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     full_name VARCHAR(255) UNIQUE NOT null,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	deleted_at TIMESTAMP
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE publisher (
     publisher_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255) UNIQUE NOT null,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	deleted_at TIMESTAMP
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE book (
     book_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    isbn VARCHAR(14) UNIQUE,
+    isbn VARCHAR(14),
     title VARCHAR(255) NOT NULL,
     publication_date DATE NOT NULL,
     publisher_id UUID REFERENCES publisher(publisher_id),
     rating NUMERIC(4, 3),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	deleted_at TIMESTAMP
+	deleted_at TIMESTAMP,
+    UNIQUE (isbn, deleted_at)
 );
 
 CREATE TABLE book_category (
     book_id UUID REFERENCES book(book_id),
     category_id UUID REFERENCES category(category_id),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	deleted_at TIMESTAMP,
     PRIMARY KEY (book_id, category_id)
 );
 
 CREATE TABLE book_author (
     book_id UUID REFERENCES book(book_id),
     author_id UUID REFERENCES author(author_id),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	deleted_at TIMESTAMP,
     PRIMARY KEY (book_id, author_id)
 );
 
